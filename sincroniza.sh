50446b995a411cf622eacd7e499e62eab5aba57b
Luciano Leonel de Medeiros <lulu.medeiros@gmail.com>
2021-02-26 15:25:16 -0300

    Update sincroniza.sh

--- sincroniza.sh
+++ sincroniza.sh
@@ -1,76 +1,149 @@
 #!/bin/bash
 
-set -xueo pipefail
+# Dependências opcionais para o script são:
+# figlet, #dnf install -y figlet
+# figlet, #dnf install -y xcowsay
+# figlet, #dnf install -y sl
 
-sudo dnf -y install fedora-workstation-repositories
+clear
 
-sudo dnf update -y
+# Setando variaveis ...
+f_lock=/tmp/lockfile # Local e arquivo de trava, para não rodar outra rotiva de backup se já houver uma ativa
 
-sudo dnf config-manager --set-enabled google-chrome
-sudo dnf -y install google-chrome-stable
-sudo dnf -y install numlockx.x86_64
-sudo dnf -y install remmina
-sudo dnf -y install xpad.x86_64
-sudo dnf -y install htop.x86_64
-sudo dnf -y install git
-sudo dnf -y install sl
-sudo dnf -y install cowsay
-sudo dnf -y install xcowsay.x86_64
-sudo dnf -y install toilet.x86_64
-sudo dnf -y install xrdp
-sudo dnf -y install xorgxrdp
-sudo dnf -y install filezilla.x86_64
-sudo dnf -y install figlet.x86_64
-sudo dnf -y install fish
-sudo dnf -y install gpick.x86_64
-sudo dnf -y install tilix.x86_64
-sudo dnf -y install gnome-software.x86_64
-sudo dnf -y install libreoffice-langpack-pt-BR.x86_64
-sudo dnf -y install libreoffice-icon-theme-papirus.noarch
-#sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
-#sudo printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
-#sudo dnf -y install codium
+function saindo {
+    
+    saindo=3 # Setando os segundos no caso de sair
+    while [ $saindo -ne 0 ]; do # Entrando no loop do contador do aviso
 
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf check-update
-sudo dnf -y install code
+        echo -e "\t Finalizando o backup em $saindo segundos. "
+        sleep 1;
+        ((saindo=$saindo-1))
+        
+    done
+    echo -e "\n\t Finalizado o script. Bye \n "
+    exit 1
 
-sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf -y install vlc
+}
 
-sudo rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
-sudo sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packagecloud.io/shiftkey/desktop/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/shiftkey/desktop/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo'
-sudo dnf -y install github-desktop
+function backup {
 
-sudo dnf -y install virt-manager bridge-utils libvirt virt-install qemu-kvm
-sudo dnf -y install virt-top libguestfs-tools
+    while : ; do
+        # Setando variaveis ...
+        o_user=$(whoami)
+        o_origem=/home/$o_user/ # Set aqui a origem dos arquivos do backup
+        d_destin=/mnt/BACKUP160/Backup-Linux/$o_user/ # Set aqui o destino dos arquivos do backup
+        f_lock=/tmp/lockfile # Local e arquivo de trava, para não rodar outra rotiva de backup se já houver uma ativa
 
-sudo dnf -y install -y python3.x86_64 \
-    python-crypto \
-    python-devel \
-    python-dns \
-    python-markdown \
-    python2-gpg \
-    python2-tkinter.x86_64 \
-    python3-tkinter.x86_64 \
-    python3 \
-    python3-crypto \
-    python3-devel \
-    python3-dns \
-    python3-gpg \
-    python3-markdown \
-    python3-ldap3.noarch \
-    python-vlc \
+        # O que não é para copiar está setado no arquivo txt
+        nao_copiar="nao_copiar.txt" # Neste caso o arquivo esta no mesmo local do script de backup
 
-sudo systemctl start xrdp
-sudo systemctl enable xrdp
+        # Configurações do aviso da vaquinha do comando xcowsay
+        conf_xcowsay="--time=4 --cow-size=small --reading-speed=2"
 
-sudo systemctl start libvirtd
-sudo systemctl enable libvirtd
+        # Verifica e avisa se a pasta de destino do backup não for encontrada e sai do script encerrando.
+        if [ ! -d $d_destin ]; then
+            xcowsay $conf_xcowsay " O diretório de destino do backup não foi encontrado! "
+            exit 1
+        fi
 
-sudo firewall-cmd --add-port=3389/tcp --permanent
-sudo firewall-cmd --reload
+        touch $f_lock # Cria o arquivo de trava
 
-sudo dnf -y clean all
+        # Começo do código a ser loopado.
+        # Seta e desmonta a data.
+        sl # Chama o trem
+        figlet -w 150 Script de Backup do Lule
+
+        ini_bk=`date +'%d/%m/%Y, às %H:%M:%S'`
+        echo -e "\n\tO backup foi iniciado em $ini_bk"
+
+        rsync -avzP --delete --exclude-from=$nao_copiar $o_origem $d_destin
+
+        fim_bk=`date +'%d/%m/%Y, às %H:%M:%S'`
+        echo -e "\n\tO backup foi iniciado  em $ini_bk"
+        echo -e "\tO backup foi concluído em $fim_bk"
+        
+        segundos=1800 # Setando os segundos para o próximo backup
+        let "minutos = $(( $segundos / 60 ))" # Setando os minutos, que são equivalentes aos segundos informados acima divididos por 60
+
+        while [ $segundos -ne 0 ]; do # Entrando no loop do contador para o próximo backup
+            
+            echo -e "\n\n\tA rotina de backup é executada a cada 30 minutos e faltam \n\t$minutos minutos ou $segundos segundos para o próximo backup.\n\tO último backup foi em: $fim_bk"
+            sleep 1m;
+            ((segundos=$segundos-60))
+            ((minutos=$minutos-1))
+
+        done
+        clear
+
+        # Pasta está sincronizada, aguardando próximo loop.
+        # Fim do código dentro do loop.
+        rm $f_lock # Deletando arquivo da trava
+
+    done
+
+}
+
+
+if [[ ! -e $f_lock ]]; then
+    
+    clear
+    echo -e "\n\t Nenhum arquivo de trava encontrado, \n\t Iniciando a rotina de backup. \n "
+    backup # Chama a função/rotina de backup
+
+else
+    
+    clear
+    echo -e "\n
+            Atenção, arquivo de trava encontrado em: \n
+            $f_lock \n
+            Parece que existe uma rotina de backup ativa, 
+            convem verificar, também pode ser uma rotina
+            mau finalizada.
+            \n"
+
+    echo -e " Escolha uma das opções abaixo, '1' para Sim e deletar a \n trava e continuar o backup ou '2' para Não terminando o \n script. \n "
+
+    # Nomes de sistemas operacionais são usados aqui como fonte de dados
+    select op in Sim Não
+
+    do
+        clear
+        case $op in
+
+            # Dois valores de caso são declarados aqui para correspondência
+            "Sim")
+            
+                clear
+                echo -e "\n\t Você digitou $op, deletando o aquivo de trava "
+                rm "$f_lock" # Deletando arquivo da trava
+                sleep 2
+                echo -e "\t A trava foi deletada, continuando para o backup. "
+                sleep 2
+                backup
+                #break
+            ;;
+
+            # Três valores de caso são declarados aqui para correspondência
+            "Não")
+
+                clear
+                echo -e "\n\t Você digitou $op, o script de backup será finalizado... "
+                saindo
+                break
+
+            ;;
+
+            # Correspondendo com dados inválidos
+            *)
+
+                clear
+                echo -e "\n\t Opção inválida, o script de backup será finalizado... "
+                saindo
+                break
+
+            ;;
+        esac
+
+    done
+
+fi
